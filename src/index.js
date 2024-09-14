12c0cc1a0a5bd474cd09dc59c944a354ba7ae915
Solomon Amsalu <solamsalu@gmail.com>
2024-09-15 01:24:29 +0300

    Update to use HashRouter for GitHub Pages routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './commonResources/css/stylesBoot.css';
 import './commonResources/css/bootstrap.css';
 import './component/footer/Jquery';
-
 import App from './App';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
